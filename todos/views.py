abb3287fb18bfd14dac22ff492e637a659ab8a60
bispobr <156684382+bispobr@users.noreply.github.com>
2025-03-07 15:29:54 -0300

    feat: adiciona crud e pagina de visualização

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Todo
 
 
-def home(request):
-    return render(request, "todos/home.html")
+class TodoListView(ListView):
+    model = Todo
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy("todo_list")
